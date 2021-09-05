81473e023c2d10afb0cef8146b00970149225448
Omid Mohajerani <omid.mohajerani@gmail.com>
2021-09-05 17:43:42 +0800

    Update s4h1.py

--- s4h1.py
+++ s4h1.py
@@ -3,6 +3,7 @@
 # We are using a fucntion with return value
 # Volume of sphere = 4.0/ 3.0 * pi * r**3
 # pi = 3.1415926535897931
+# Output https://github.com/Omid-Mohajerani/pyclass/wiki/PYTHON-HOMEWORKS#s4h1--calculate-volume-of-a-sphere-using-function-with-return-value
 
 
 def sphere_volume(r):
