056896c72827b3914e384f05ed1afa768036cf13
Omid Mohajerani <omid.mohajerani@gmail.com>
2021-09-16 13:15:33 +0800

    Add files via upload

---
+++ s5h1.py
@@ -0,0 +1,8 @@
+# Omid Mohajearni - Python Class -  Thursdays -  14 to 18
+# adding 3 lists and print it in reverse with one pass
+# Output: 
+list1 = [1,2,3]
+list2 = [4,5,6]
+list3 = [7,8,9]
+newlist = list1[:] + list2[:] + list3[:]
+print(newlist[::-2])
