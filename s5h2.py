7154c1c994926a1c8952349611495679e6a237af
Omid Mohajerani <omid.mohajerani@gmail.com>
2021-09-16 13:23:35 +0800

    Add files via upload

---
+++ s5h2.py
@@ -0,0 +1,7 @@
+# Omid Mohajearni - Python Class -  Thursdays -  14 to 18
+# add 3 lists and print in reversed Decussated ( in one line of code)
+# Output:
+list1 = [1,2,3]
+list2 = [4,5,6]
+list3 = [7,8,9]
+print((list1[:]+list2[:]+list3[:])[::-2])
