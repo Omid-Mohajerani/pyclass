b66f100d87fde4ea1ef51ff37a201a2c54393c2b
Omid Mohajerani <omid.mohajerani@gmail.com>
2021-09-16 15:05:10 +0800

    Add files via upload

---
+++ s5h3.py
@@ -0,0 +1,18 @@
+# Omid Mohajearni - Python Class -  Thursdays -  14 to 18
+# Ask a list of fruits from User and show number of unique fruis
+# Output:
+
+firstfruit = input("Enter name of a fruit: ")
+fruits = []
+fruits.append(firstfruit.title())
+condition = "True"
+while condition == "True":
+    nextfruite = input("Enter name of another fruite or enter 'End' to finish: ")
+    if nextfruite.title() == "End":
+        condition = "False"
+        break
+    fruits.append(nextfruite.title())
+    print(f"All fruits:    {fruits}")
+    uniquefruits = set(fruits)
+    print(f"Unique fruits: {uniquefruits}")
+print(f"You have entered {len(fruits)} fruits that {len(uniquefruits)} are unique")
