3af35439e21400da6795d30ef32bfe8d2d82dd26
Omid Mohajerani <omid.mohajerani@gmail.com>
2021-09-16 13:25:13 +0800

    Update s5h2.py

--- s5h2.py
+++ s5h2.py
@@ -1,6 +1,6 @@
 # Omid Mohajearni - Python Class -  Thursdays -  14 to 18
 # add 3 lists and print in reversed Decussated ( in one line of code)
-# Output:
+# Output: https://github.com/Omid-Mohajerani/pyclass/wiki/PYTHON-HOMEWORKS#s5h2-add-3-lists-and-print-in-reversed-decussated--in-one-line-of-code
 list1 = [1,2,3]
 list2 = [4,5,6]
 list3 = [7,8,9]
