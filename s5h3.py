e4f1c0a91b56014c292d2a5393f7d45ee8838a9e
Omid Mohajerani <omid.mohajerani@gmail.com>
2021-09-16 15:06:49 +0800

    Update s5h3.py

--- s5h3.py
+++ s5h3.py
@@ -1,5 +1,5 @@
 # Omid Mohajearni - Python Class -  Thursdays -  14 to 18
-# Ask a list of fruits from User and show number of unique fruits
+# Ask a list of fruits from the User and show the number of unique fruits
 # Output:
 
 firstfruit = input("Enter name of a fruit: ")
